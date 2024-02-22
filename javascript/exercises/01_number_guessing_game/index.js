595becdd0626e8e544be6f9a764c405be7300685
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2024-02-22 15:13:28 +0200

    Exercise 1

--- javascript/exercises/01_number_guessing_game/index.js
+++ javascript/exercises/01_number_guessing_game/index.js
@@ -1,40 +1,43 @@
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+var tries = 0;
+var lowest = 0;
+var highest = 100;
+
 window.onload = function () {
     const form = document.getElementById("form");
     form.addEventListener("submit", submit);
     form.addEventListener("reset", reset);
-    randomNumber = Math.floor(Math.random() * 100 + 1);
-    tries = 0;
-    lastGuess = 0;
+
+    document.getElementById("number").focus();
 }
 
 function submit(event) {
     event.preventDefault();
     let hint = document.querySelector("#hint");
-    let guessedNumber = document.querySelector("#number").value;
+    let guessedNumber = parseInt(document.querySelector("#number").value);
 
     if (isNaN(guessedNumber) || guessedNumber < 1 || guessedNumber > 100) {
         hint.innerHTML = "Choose a number between 1 and 100!";
     } else if (guessedNumber > randomNumber) {
-        if (guessedNumber >= lastGuess && lastGuess != 0) {
-            hint.innerHTML = "I SAID LOWER!";
-        } else {
+        if (guessedNumber < highest) {
             hint.innerHTML = "Lower!";
+            highest = guessedNumber;
             tries++;
-            lastGuess = guessedNumber;
+        } else {
+            hint.innerHTML = "I SAID LOWER!";
         }
     } else if (guessedNumber < randomNumber) {
-        if (guessedNumber <= lastGuess && lastGuess != 0) {
-            hint.innerHTML = "I SAID HIGHER!";
-        } else {
+        if (guessedNumber > lowest) {
             hint.innerHTML = "Higher!";
+            lowest = guessedNumber;
             tries++;
-            lastGuess = guessedNumber;
+        } else {
+            hint.innerHTML = "I SAID HIGHER!";
         }
     } else if (guessedNumber == randomNumber) {
         hint.innerHTML = "CORRECT! COOKIES!";
     }
 
-    console.log(lastGuess);
     document.querySelector("#tries").innerHTML = tries;
 
     document.getElementById("number").value = "";
