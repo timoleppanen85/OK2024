659181f559bf27e251c73dbbf3f456af591b5189
timoleppanen85 <31985525+timoleppanen85@users.noreply.github.com>
2024-02-22 13:41:54 +0200

    Task 7 done

--- javascript/07_simple_spa_1/public/script.js
+++ javascript/07_simple_spa_1/public/script.js
@@ -4,6 +4,7 @@ var mode = 0;
 
 window.onload = function () {
     createForm();
+    populateTable();
 }
 
 createForm = () => {
@@ -28,12 +29,12 @@ createForm = () => {
     // Last name input and label
     let lastNameInput = document.createElement("input");
     lastNameInput.setAttribute("type", "text");
-    lastNameInput.setAttribute("name", "lastName");
-    lastNameInput.setAttribute("id", "lastName");
+    lastNameInput.setAttribute("name", "lastname");
+    lastNameInput.setAttribute("id", "lastname");
     lastNameInput.setAttribute("class", "form-control");
 
     let lastNameLabel = document.createElement("label");
-    lastNameLabel.setAttribute("for", "lastName");
+    lastNameLabel.setAttribute("for", "lastname");
     lastNameLabel.setAttribute("class", "form-label");
 
     let lastNameText = document.createTextNode("Last Name");
@@ -77,7 +78,168 @@ createForm = () => {
     form.append(firstNameLabel, firstNameInput, lastNameLabel, lastNameInput, emailLabel, emailInput, phoneLabel, phoneInput, submitButton);
     form.addEventListener("submit", function (event) {
         event.preventDefault();
+        addContact();
     })
 
     root.appendChild(form);
+}
+
+addContact = () => {
+    const firstNameInput = document.getElementById("firstname");
+    const lastNameInput = document.getElementById("lastname");
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone");
+
+    let contact = {
+        "firstname": firstNameInput.value,
+        "lastname": lastNameInput.value,
+        "email": emailInput.value,
+        "phone": phoneInput.value
+    }
+
+    if (mode) {
+        contact.id = mode;
+
+        for (let i = 0; i < database.length; i++) {
+            if (contact.id === database[i].id) {
+                database.splice(i, 1, contact);
+            }
+        }
+    } else {
+        contact.id = id;
+        id++;
+        database.push(contact);
+    }
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    phoneInput.value = "";
+
+    const submitButton = document.getElementById("submitbutton");
+    submitButton.value = "Add";
+
+    mode = 0;
+
+    populateTable();
+}
+
+removeContact = (id) => {
+    for (let i = 0; i < database.length; i++) {
+        if (id === database[i].id) {
+            database.splice(i, 1);
+        }
+    }
+
+    populateTable();
+}
+
+editContact = (contact) => {
+    const firstNameInput = document.getElementById("firstname");
+    const lastNameInput = document.getElementById("lastname");
+    const emailInput = document.getElementById("email");
+    const phoneInput = document.getElementById("phone");
+
+    firstNameInput.value = contact.firstname;
+    lastNameInput.value = contact.lastname;
+    emailInput.value = contact.email;
+    phoneInput.value = contact.phone;
+
+    mode = contact.id;
+
+    const submitButton = document.getElementById("submitbutton");
+    submitButton.value = "Save";
+}
+
+populateTable = () => {
+    const root = document.getElementById("root");
+    const oldTable = document.getElementById("table");
+
+    if (oldTable) {
+        root.removeChild(oldTable);
+    }
+
+    const table = document.createElement("table");
+    table.setAttribute("class", "table table-striped");
+    table.setAttribute("id", "table");
+
+    // Table Headers
+    const header = document.createElement("thead");
+    const headerRow = document.createElement("tr");
+
+    // First name Header
+    const firstNameHeader = document.createElement("th");
+    const firstNameNode = document.createTextNode("First Name");
+    firstNameHeader.appendChild(firstNameNode);
+
+    // Last name Header
+    const lastNameHeader = document.createElement("th");
+    const lastNameNode = document.createTextNode("Last Name");
+    lastNameHeader.appendChild(lastNameNode);
+
+    // Email Header
+    const emailHeader = document.createElement("th");
+    const emailNode = document.createTextNode("Email");
+    emailHeader.appendChild(emailNode);
+
+    // Phone Header
+    const phoneHeader = document.createElement("th");
+    const phoneNode = document.createTextNode("Phone");
+    phoneHeader.appendChild(phoneNode);
+
+    // Remove Header
+    const removeHeader = document.createElement("th");
+    const removeNode = document.createTextNode("Remove");
+    removeHeader.appendChild(removeNode);
+
+    // Edit Header
+    const editHeader = document.createElement("th");
+    const editNode = document.createTextNode("Edit");
+    editHeader.appendChild(editNode);
+
+    headerRow.append(firstNameHeader, lastNameHeader, emailHeader, phoneHeader, removeHeader, editHeader);
+    header.appendChild(headerRow);
+    table.appendChild(header);
+
+    // Table body
+    const body = document.createElement("tbody");
+
+    for (let i = 0; i < database.length; i++) {
+        const row = document.createElement("tr");
+        for (x in database[i]) {
+            if (x === "id") {
+                continue;
+            }
+
+            const column = document.createElement("td");
+            const info = document.createTextNode(database[i][x]);
+            column.appendChild(info);
+            row.append(column);
+        }
+
+        const removeColumn = document.createElement("td");
+        const removeButton = document.createElement("button");
+        removeButton.setAttribute("class", "btn btn-danger");
+        const removeButtonText = document.createTextNode("Remove");
+        removeButton.appendChild(removeButtonText);
+        removeButton.addEventListener("click", function (event) {
+            removeContact(database[i].id);
+        })
+
+        const editColumn = document.createElement("td");
+        const editButton = document.createElement("button");
+        editButton.setAttribute("class", "btn btn-secondary");
+        const editButtonText = document.createTextNode("Edit");
+        editButton.appendChild(editButtonText);
+        editButton.addEventListener("click", function (event) {
+            editContact(database[i]);
+        })
+
+        removeColumn.appendChild(removeButton);
+        editColumn.appendChild(editButton);
+        row.append(removeColumn, editColumn);
+        body.appendChild(row);
+    }
+
+    table.appendChild(body);
+    root.appendChild(table);
 }
